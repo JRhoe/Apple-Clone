fc04349fed57fe21098fde5053e86e3704defe87
James <jamesrhoe1@gmail.com>
2024-07-21 21:56:12 -0700

    initial commit of apple web clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Features from "./componants/Features";
+import Footer from "./componants/Footer";
+import Hero from "./componants/Hero";
+import Highlights from "./componants/Highlights";
+import HowItWorks from "./componants/HowItWorks";
+import Model from "./componants/Model";
+import Navbar from "./componants/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </div>
   );
 }
